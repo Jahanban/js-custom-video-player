9ebd5c135abe1e3f4fc56fd56ca76f2366471501
Jahanban <jahanban@gmail.com>
2020-07-25 14:40:08 -0400

    pause functionality added

--- script.js
+++ script.js
@@ -4,6 +4,44 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+// Play and Pause function
+
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+// update pay and pause icon
+
+function updatePlayIcon() {
+    if (video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>'
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
+    }
+}
+
+// update progress and timestamp
+
+function updateProgress() {
+    return true;
+}
+
+// set video time to progress
+
+function setVideoProgress() {
+    return true;
+}
+
+// stop the video
+
+function stopVideo() {
+    return true;
+}
+
 //Event Listeners
 
 video.addEventListener('click', toggleVideoStatus);
