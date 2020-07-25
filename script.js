ca5b3e4c2a71c9d9bcd7c56200678a730f1fe30d
Jahanban <jahanban@gmail.com>
2020-07-25 16:28:25 -0400

    Custom video player conplete with timestamp updating and pause and stop functionality

--- script.js
+++ script.js
@@ -27,19 +27,38 @@ function updatePlayIcon() {
 // update progress and timestamp
 
 function updateProgress() {
-    return true;
+    progress.value = (video.currentTime / video.duration) * 100;
+
+    // get the minutes
+
+    let mins = Math.floor(video.currentTime / 60);
+
+    if (mins < 10) {
+        mins = '0' + String(mins);
+    }
+
+    //Get seconds
+
+    let secs = Math.floor(video.currentTime % 60);
+
+    if (secs < 10) {
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`
 }
 
 // set video time to progress
 
 function setVideoProgress() {
-    return true;
+    video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // stop the video
 
 function stopVideo() {
-    return true;
+    video.currentTime = 0;
+    video.paused();
 }
 
 //Event Listeners
